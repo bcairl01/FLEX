ddac98a268ad51c734d49df67ead2b7cc17c76ce
Brian Cairl <briancairl@gmail.com>
2014-07-12 22:56:03 -0400

    Added typedefs. Moved some stuff from y EXI wrapper headers

--- FLEX.h
+++ FLEX.h
@@ -3,33 +3,50 @@
 #include    "stdint.h"
 
 
+/// @ingroup flexTypedefs
+/// @{
 
-typedef void(*flexTXService)    (uint8_t);
-typedef uint8_t(*flexRXService) (void);
+    typedef void(*flexTXService)    (uint8_t);
+    typedef uint8_t(*flexRXService) (void);
 
+    /// @brief  Generic Ring-Buffer Structure
+    /// @var    rd_idx          next slot in buffer to read from
+    /// @var    wr_idx          next slot in buffer to write to
+    /// @var    size            number of unread bytes in buffer
+    /// @var    data            buffer stack space
+    /// @var    overflow        buffer-overflow flag
+    typedef struct {
+        uint16_t                rd_idx;
+        uint16_t                wr_idx;
+        uint16_t                size;
+        uint8_t                 data[2048UL];
+        uint8_t                 flags;
+    } flexBuffer_t;
+
+/// @}
+
+
+
+
+/// @ingroup flexAPI
+/// @{
 
 /// @ brief defines the start of a flex-tree structure
 #define FLEXTREE_BEGIN(tag)                                             \
-static union __flexdef_Tree_##tag                                       \
-{                                                                       \
-    struct __flexdef_Struct_##tag                                       \
-    {
+    static union  __flexdef_Tree_##tag                                  \
+    {                                                                   \
+        struct __flexdef_Struct_##tag                                   \
+        {
+
 
 
 
 /// @brief Defines the end of a flexTree structure
 #define FLEXTREE_END(tag)                                               \
-    } flexdef_Struct_##tag;                                             \
-    uint8_t flexdef_cbuf[sizeof(struct __flexdef_Struct_##tag)];        \
-} flexdef_Tree_##tag;                                                   \
-                                                                        \
-static void flexTransmit_##tag( flexTXService _TX )                     \
-{                                                                       \
-    uint16_t size = sizeof(flexdef_Tree_##tag.flexdef_Struct_##tag);    \
-    uint8_t* cbuf_ptr = (flexdef_Tree_##tag).flexdef_cbuf;              \
-    while(size--)                                                       \
-        _TX(*(cbuf_ptr++));                                             \
-}
+        } flexdef_Struct_##tag;                                         \
+        uint8_t flexdef_cbuf[sizeof(struct __flexdef_Struct_##tag)];    \
+    } flexdef_Tree_##tag;                                               \
+
 
 
 
@@ -39,15 +56,24 @@ static void flexTransmit_##tag( flexTXService _TX )                     \
 
 
 
-/// @brief Get a variable from the flexTree
-#define FLEXTREE_GET(tag,varname)                                       \
-    (flexdef_Tree_##tag.flexdef_Struct_##tag).varname
-    
 
-    
+/// @brief Get a variable from the flexTree
+#define FLEXTREE_GETVAR(tag,varname)                                    \
+    (flexdef_Tree_##tag.flexdef_Struct_##tag).varname
+
+
+
+
 /// @brief Gets the size of the flexTree in bytes
-#define FLEXTREE_SIZE(tag)                                              \
+#define FLEXTREE_GETSIZE(tag)                                           \
     sizeof(flexdef_Tree_##tag.__flexdef_Struct_##tag)
 
+
+
+/// @brief Gets the current output buffer corresponding to the target flexTree
+#define FLEXTREE_GETBUG(tag)                                            \
+    flexdef_Tree_##tag.flexdef_cbuf
+
+/// @}
 
 #endif
