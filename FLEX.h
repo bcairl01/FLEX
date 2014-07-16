25317c10c85ba50e8feb72a65c5688a22ab7e8c5
Brian Cairl <briancairl@gmail.com>
2014-07-16 03:32:01 -0400

    Added buffer_init to FLEXTREE_SETUP macro.

--- FLEX.h
+++ FLEX.h
@@ -1,68 +1,51 @@
 #ifndef     FLEX_H
 #define     FLEX_H
 #include    "stdint.h"
+#include    "cbuffer.h"
 
 
-/// @ingroup flexTypedefs
+/// @ingroup FLEXTypedefs
 /// @{
 
-typedef void(*flexTXService)   (uint8_t);
-typedef uint8_t(*flexRXService)(void);
+    typedef void(*FLEXTXService)   (uint8_t);
 
-#define FLEXBUFFER_CAP                      2048UL
 
-/// @brief  Generic Ring-Buffer Structure
-/// @var    rd_idx          next slot in buffer to read from
-/// @var    wr_idx          next slot in buffer to write to
-/// @var    size            number of unread bytes in buffer
-/// @var    data            buffer stack space
-/// @var    overflow        buffer-overflow flag
-typedef struct {
-    uint16_t                rd_idx;
-    uint16_t                wr_idx;
-    uint16_t                pr_idx;
-    uint16_t                size;
-    uint8_t                 flags;
-    uint8_t                 data[FLEXBUFFER_CAP];
-} flexBuffer_t;
-#define FLEXBUFFER_INITIALIZER              {0,0,0,0}
-#define FLEXBUFFER_RDPP(buf_p)              if(buf_p->size > 0UL           ) { --buf_p->size; ++buf_p->rd_idx; buf_p->rd_idx%=FLEXBUFFER_CAP; }
-#define FLEXBUFFER_WRPP(buf_p)              if(buf_p->size < FLEXBUFFER_CAP) { ++buf_p->size; ++buf_p->wr_idx; buf_p->wr_idx%=FLEXBUFFER_CAP; }
-#define FLEXBUFFER_SIZE(buf_p)              buf_p->size
-#define FLEXBUFFER_RD_AT(buf_p)             buf_p->data[buf_p->rd_idx]
-#define FLEXBUFFER_WR_AT(buf_p)             buf_p->data[buf_p->wr_idx]
+    typedef struct _FLEXRXStat
+    {
+        uint16_t    head_itr;
+        uint16_t    data_itr;
+        uint16_t    tail_itr;
+        uint8_t     checksum;
+        uint8_t     checkbuf[2048UL];
+    } FLEXRXStat_t;
+
+
+    #define FLEXRXSTAT_RESET(stat_p)                                                            \
+        stat_p->head_itr    =                                                                   \
+        stat_p->tail_itr    =                                                                   \
+        stat_p->data_itr    =                                                                   \
+        stat_p->checksum    = 0;
+
+
+    #define FLEXRXSTAT_COPY(stat_p,userp)                                                       \
+        while(stat_p->data_itr--)                                                               \
+            userp[stat_p->data_itr] = stat_p->checkbuf[stat_p->data_itr]
+
+
 /// @}
 
 
 
-/// @ingroup flexRXStat
+
+
+/// @ingroup FLEXBase
 /// @{
-typedef struct _flexRXStat
-{
-    uint16_t    head_itr;
-    uint16_t    data_itr;
-    uint16_t    tail_itr;
-    uint8_t     checksum;
-    uint8_t     temp_buf[2048];
-} flexRXStat_t;
-
-#define FLEXRXSTAT_RESET(stat_p)                                                            \
-    stat_p->head_itr    =                                                                   \
-    stat_p->tail_itr    =                                                                   \
-    stat_p->data_itr    =                                                                   \
-    stat_p->checksum    = 0;
-
-#define FLEXRXSTAT_COPY(stat_p,userp)                                                       \
-    while(stat_p->data_itr--)                                                               \
-        userp[stat_p->data_itr] = stat_p->temp_buf[stat_p->data_itr]
-/// @}
 
 
+    void    FLEXBASE_SendTree( uint8_t userp[], uint16_t size, FLEXTXService _TX );
+    
 
-/// @ingroup flexBase
-/// @{
-void    FLEXBASE_SendTree( uint8_t userp[], uint16_t size, flexTXService _TX );
-uint8_t FLEXBASE_RecvTree( uint8_t userp[], uint16_t size, flexRXService _RX, flexRXStat_t* _RXStat );
+    uint8_t FLEXBASE_UpdateTree( uint8_t userp[], uint16_t size, cbuffer* buf_p, FLEXRXStat_t* _RXStat );
 
 
 /// @}
@@ -72,82 +55,99 @@ uint8_t FLEXBASE_RecvTree( uint8_t userp[], uint16_t size, flexRXService _RX, fl
 
 
 
-/// @ingroup flexAPI
+/// @ingroup FLEXAPI
 /// @{
 
 
-/// @ brief defines the start of a flexTree structure
-#define FLEXTREE_BEGIN(tag)                                                                 \
-    static flexRXStat_t     flexDef_RecvSupports_##tag          = {0,0,0,0};                \
-    static union  __flexdef_Tree_##tag                                                      \
-    {                                                                                       \
-        struct __flexdef_Struct_##tag                                                       \
-        {
+    /// @ brief defines the start of a FLEXTree structure
+    #define FLEXTREE_BEGIN(tag)                                                                 \
+        static FLEXRXStat_t     FLEXDef_RecvSupports_##tag          = {0,0,0,0};                \
+        static cbuffer          FLEXDef_Buffer_##tag;                                           \
+        static union  __FLEXDEF_Tree_##tag                                                      \
+        {                                                                                       \
+            struct __FLEXDEF_Struct_##tag                                                       \
+            {
 
 
 
 
-/// @brief Defines the end of a flexTree structure
-#define FLEXTREE_END(tag)                                                                   \
-        } flexdef_Struct_##tag;                                                             \
-        uint8_t flexdef_cbuf[sizeof(struct __flexdef_Struct_##tag)];                        \
-    } flexdef_Tree_##tag;                                                                   \
+    /// @brief Defines the end of a FLEXTree structure
+    #define FLEXTREE_END(tag)                                                                   \
+            } FLEXDEF_Struct_##tag;                                                             \
+            uint8_t FLEXDEF_cbuf[sizeof(struct __FLEXDEF_Struct_##tag)];                        \
+        } FLEXDEF_Tree_##tag;                                                                   \
 
 
 
 
-/// @brief Sends the flexTree using the provided tx-service
-#define FLEXTREE_SEND(tag,tx_fn)                                                            \
-    FLEXBASE_SendTree(                                                                      \
-        flexdef_Tree_##tag.flexdef_cbuf,                                                    \
-        sizeof(flexdef_Tree_##tag.flexdef_Struct_##tag),                                    \
-        tx_fn                                                                               \
-    )
-
-
-
-/// @brief Sends the flexTree using the provided rx-service
-#define FLEXTREE_RECV(tag,rx_fn)                                                            \
-    FLEXBASE_RecvTree(                                                                      \
-        flexdef_Tree_##tag.flexdef_cbuf,                                                    \
-        sizeof(flexdef_Tree_##tag.flexdef_Struct_##tag),                                    \
-        rx_fn,                                                                              \
-    &   flexDef_RecvSupports_##tag                                                          \
-    )
+    /// @brief Initializes FLEXTree support structures. To be called AFTER a FLEXTree is defined
+    #define FLEXTREE_SETUP(tag,size)                                                            \
+        buffer_init(&FLEXDef_Buffer_##tag,size);                                                \
+        buffer_alloc(&FLEXDef_Buffer_##tag,size);
 
 
 
 
-/// @brief Adds a variable to the flexTree
-#define FLEXTREE_VAR(_ty,varname)                                                           \
-        _ty varname;
+    /// @brief Deletes FLEXTree support structures. To be called AFTER a FLEXTree is defined
+    #define FLEXTREE_CLEAR(tag)                                                                 \
+        buffer_free(&FLEXDef_Buffer_##tag)
 
 
 
 
-/// @brief Get a variable from the flexTree
-#define FLEXTREE_GETVAR(tag,varname)                                                        \
-    (flexdef_Tree_##tag.flexdef_Struct_##tag).varname
+    /// @brief Sends the FLEXTree using the provided tx-service
+    #define FLEXTREE_SEND(tag,tx_handle)                                                        \
+        FLEXBASE_SendTree(                                                                      \
+            FLEXDEF_Tree_##tag.FLEXDEF_cbuf,                                                    \
+            sizeof(FLEXDEF_Tree_##tag.FLEXDEF_Struct_##tag),                                    \
+            tx_handle                                                                           \
+        )
 
 
 
 
-/// @brief Gets the size of the flexTree in bytes
-#define FLEXTREE_GETSIZE(tag)                                                               \
-    sizeof(flexdef_Tree_##tag.flexdef_Struct_##tag)
+    /// @brief Updates the FLEXTree using data from its associated rx-buffer
+    #define FLEXTREE_UPDATE(tag)                                                                \
+        FLEXBASE_UpdateTree(                                                                    \
+            FLEXDEF_Tree_##tag.FLEXDEF_cbuf,                                                    \
+            sizeof(FLEXDEF_Tree_##tag.FLEXDEF_Struct_##tag),                                    \
+        &   FLEXDef_Buffer_##tag,                                                               \
+        &   FLEXDef_RecvSupports_##tag                                                          \
+        )
 
 
 
-/// @brief Gets the current output buffer corresponding to the target flexTree
-#define FLEXTREE_GETBUF(tag)                                                                \
-    flexdef_Tree_##tag.flexdef_cbuf
+    /// @brief Adds a variable to the FLEXTree
+    #define FLEXTREE_VAR(_ty,varname)                                                           \
+            _ty varname;
 
 
 
-/// @brief Set a variable from a flexTree value
-#define FLEXTREE_SETFROMVAR(tag,varname,this)                                               \
-    this = (flexdef_Tree_##tag.flexdef_Struct_##tag).varname
 
+    /// @brief Get a variable from the FLEXTree
+    #define FLEXTREE_GETVAR(tag,varname)                                                        \
+        (FLEXDEF_Tree_##tag.FLEXDEF_Struct_##tag).varname
+
+
+
+
+    /// @brief Gets the size of the FLEXTree in bytes
+    #define FLEXTREE_GETSIZE(tag)                                                               \
+        sizeof(FLEXDEF_Tree_##tag.FLEXDEF_Struct_##tag)
+
+
+
+
+    /// @brief Sets byte to recieve buffer
+    #define FLEXTREE_RXBYTE(tag,byte)                                                           \
+        buffer_put(&FLEXDef_Buffer_##tag,byte)
+
+
+
+
+    /// @brief Gets the status of the FLEXTree. Returns '1' is all is well
+    #define FLEXTREE_OK(tag)                                                                    \
+        BUFFER_OK(&FLEXDef_Buffer_##tag)
 
 
 /// @}
